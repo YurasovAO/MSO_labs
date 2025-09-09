b4dce78744e16b00d8c1f9b7f25dbbe7dff4f7a6
YurasovAO <YurasovAO@mpei.ru>
2025-09-09 14:26:50 -0400

    lab 2 part11 in progress

---
+++ lab2/part11.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#export DIGIT_FOR_LAB=3
+#export MYOWNNAME="My name is Artem"
+#export TIMEZONE="EDT"
+#export MYGROUP="MY GROUP is A-04M-25"
+#export DAY_OF_WEEK_TODAY=2
+
+
+
+
+$(source ~/.bashrc)
